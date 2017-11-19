668f35114ba75f7e79cf44b413f7c97d0d87d215
J-Mangia <j.mangia@ibm.com>
2017-11-18 16:14:46 -0800

    Added tableview for last week HRV values

--- WatchHRV/TableViewController.swift
+++ WatchHRV/TableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TableViewController.swift
 //  WatchHRV
 //
 //  Created by Jacopo Mangiavacchi on 11/18/17.
@@ -9,15 +9,15 @@
 import UIKit
 import HealthKit
 
-class ViewController: UIViewController {
+class TableViewController: UITableViewController {
 
     let healthStore = HKHealthStore()
     let hrvUnit = HKUnit(from: "ms")
-    
-    
+    var hrvData = [HKQuantitySample]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         guard HKHealthStore.isHealthDataAvailable() == true else {
             print("not available")
             return
@@ -50,26 +50,51 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    
     func createheartRateVariabilitySDNNStreamingQuery(_ startDate: Date) -> HKQuery? {
         let typeHRV = HKQuantityType.quantityType(forIdentifier: .heartRateVariabilitySDNN)
         let predicate: NSPredicate? = HKQuery.predicateForSamples(withStart: startDate, end: nil, options: HKQueryOptions.strictStartDate)
-
+        
         let squery = HKSampleQuery(sampleType: typeHRV!, predicate: predicate, limit: 10, sortDescriptors: nil) { (query, samples, error) in
             DispatchQueue.main.async(execute: {() -> Void in
                 guard error == nil, let hrvSamples = samples as? [HKQuantitySample] else {return}
-
-                for sample in hrvSamples {
-                    let value = sample.quantity.doubleValue(for: self.hrvUnit)
-                    print("got: \(value) - \(sample.startDate)")
-                }
+                
+                self.hrvData.append(contentsOf: hrvSamples)
+                self.tableView.reloadData()
             })
         }
-
+        
         return squery
     }
-}
+    
+    // MARK: - Table view data source
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return hrvData.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "hrvCell", for: indexPath)
+
+        let sample = hrvData[indexPath.row]
+        let value = sample.quantity.doubleValue(for: self.hrvUnit)
+        let date = sample.startDate
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM dd hh:mm"
+        let todaysDate = dateFormatter.string(from: date)
+        
+        cell.textLabel?.text = String(format: "%.1f", value)
+        cell.detailTextLabel?.text = todaysDate
+        
+        return cell
+    }
+}
