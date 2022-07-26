e5050bb021548a5cd336dbc92680e1131d7ba8e5
Jon Chui <jon.chui@gmail.com>
2022-07-26 12:11:33 -0600

    Revert "MILESTONE: copied code over from Apple iPhone app, and got it working on my Apple Watch. WOHOO!!!"
    This reverts commit 4d450d41b6c4961a0bbfe72b8bfef01dc2a0424b.
    
    OPPS - my bad... was not supposed to be on this branch. But i'll leave the history, so we can CONFIRM the code to make HRV data show on the appl ewatch @ wants to merge 3 commits into JacopoMangiavacchi

--- WatchHRV WatchKit Extension/InterfaceController.swift
+++ WatchHRV WatchKit Extension/InterfaceController.swift
@@ -21,9 +21,6 @@ class InterfaceController: WKInterfaceController {
     let heartRateUnit = HKUnit(from: "count/min")
     var hrvQuery : HKQuery?
     var heartRateQuery : HKQuery?
-    
-    // from iphone
-    var hrvData = [HKQuantitySample]()
 
     @IBOutlet private weak var startStopButton : WKInterfaceButton!
     @IBOutlet private weak var hrvLabel: WKInterfaceLabel!
@@ -53,12 +50,6 @@ class InterfaceController: WKInterfaceController {
         healthStore.requestAuthorization(toShare: nil, read: dataTypes) { (success, error) -> Void in
             if success {
                 self.authorized = true
-                
-                
-                // run the HRV Query
-                let day = Date(timeIntervalSinceNow: -7*24*60*60)
-                self.hrvQuery = self.createheartRateVariabilitySDNNStreamingQuery(day)
-                self.healthStore.execute(self.hrvQuery!)
             }
             else {
                 self.displayNotAllowed()
@@ -113,38 +104,6 @@ class InterfaceController: WKInterfaceController {
         healthStore.start(self.session!)
     }
     
-    // FROM THE IPHONE APP - working
-    func createheartRateVariabilitySDNNStreamingQuery(_ startDate: Date) -> HKQuery {
-        let typeHRV = HKQuantityType.quantityType(forIdentifier: .heartRateVariabilitySDNN)
-        let predicate: NSPredicate? = HKQuery.predicateForSamples(withStart: startDate, end: nil, options: HKQueryOptions.strictStartDate)
-        
-        let squery = HKSampleQuery(sampleType: typeHRV!, predicate: predicate, limit: HKObjectQueryNoLimit, sortDescriptors: nil) { (query, samples, error) in
-            DispatchQueue.main.async(execute: {() -> Void in
-                guard error == nil, let hrvSamples = samples as? [HKQuantitySample] else {return}
-                
-                self.hrvData.append(contentsOf: hrvSamples)
-                self.hrvData.reverse();
-                // do some UI updates here? to make it show
-                
-                // print out the value onto the watch
-                let sample = self.hrvData[0]
-                let value = sample.quantity.doubleValue(for: self.hrvUnit)
-                let date = sample.startDate
-                
-                let dateFormatter = DateFormatter()
-                dateFormatter.dateFormat = "MMM dd hh:mm"
-                let todaysDate = dateFormatter.string(from: date)
-                
-                self.hrvLabel.setText(String(format: "%.1f", value))
-                self.heartRatelabel.setText(todaysDate)
-            })
-        }
-        
-        return squery
-    }
-    
-    
-    // WATCH VERSION - HRV NOT WORKING
     func getQuery(date: Date, identifier: HKQuantityTypeIdentifier) -> HKQuery? {
         guard let quantityType = HKObjectType.quantityType(forIdentifier: identifier) else { return nil }
         
@@ -172,12 +131,10 @@ class InterfaceController: WKInterfaceController {
             switch sample.quantityType {
             case heartRateQuantityType:
                 let value = sample.quantity.doubleValue(for: self.heartRateUnit)
-                print("hr: \(value)");
                 self.heartRatelabel.setText(String(format: "%.1f", value))
                 break
             case hrvQuantityType:
                 let value = sample.quantity.doubleValue(for: self.hrvUnit)
-                print("hrv: \(value)");
                 self.hrvLabel.setText(String(format: "%.1f", value))
                 break
             default:
@@ -205,19 +162,19 @@ extension InterfaceController: HKWorkoutSessionDelegate {
     
     
     func workoutDidStart(_ date : Date) {
-//        if let query = getQuery(date: date, identifier: HKQuantityTypeIdentifier.heartRate) {
-//            self.heartRateQuery = query
-//            healthStore.execute(query)
-//        } else {
-//            heartRatelabel.setText("/")
-//        }
-//
-//        if let query = getQuery(date: date, identifier: HKQuantityTypeIdentifier.heartRateVariabilitySDNN) {
-//            self.hrvQuery = query
-//            healthStore.execute(query)
-//        } else {
-//            hrvLabel.setText("/")
-//        }
+        if let query = getQuery(date: date, identifier: HKQuantityTypeIdentifier.heartRate) {
+            self.heartRateQuery = query
+            healthStore.execute(query)
+        } else {
+            heartRatelabel.setText("/")
+        }
+
+        if let query = getQuery(date: date, identifier: HKQuantityTypeIdentifier.heartRateVariabilitySDNN) {
+            self.hrvQuery = query
+            healthStore.execute(query)
+        } else {
+            hrvLabel.setText("/")
+        }
     }
     
     func workoutDidEnd(_ date : Date) {
--- WatchHRV/TableViewController.swift
+++ WatchHRV/TableViewController.swift
@@ -76,7 +76,6 @@ class TableViewController: UITableViewController {
                 guard error == nil, let hrvSamples = samples as? [HKQuantitySample] else {return}
                 
                 self.hrvData.append(contentsOf: hrvSamples)
-                self.hrvData.reverse();
                 self.refreshControl?.endRefreshing()
                 self.tableView.reloadData()
             })
